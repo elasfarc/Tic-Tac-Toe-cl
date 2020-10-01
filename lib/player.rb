fbb7d01c47e9c1e43ce284013d40d53b2290575c
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2020-10-01 18:44:51 +0200

    create Player Class and initialize method
    in main -> take players inputs -> store it

---
+++ lib/player.rb
@@ -0,0 +1,26 @@
+class Player
+    @@players_arry = []
+    attr_accessor :name
+
+    def initialize(n = "unknown_player")
+        @name = n
+        @@players_arry.push(@name)
+    end
+
+    # def print_players
+    #     @@players_arry
+    # end
+end
+
+
+
+# pp "enter ur name"
+#   p1 = Player.new(gets.chomp)
+# pp "enter ur  2nd name"
+#   p2 = Player.new(gets.chomp)
+#   p2_name = p2.name
+
+#  #pp p1_name
+
+#  pp p1.print_players
+ 
