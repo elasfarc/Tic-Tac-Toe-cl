27277cfe48d5bade5ebccf8ac708aade2acaef72
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2020-10-02 03:24:42 +0200

    Board class -> define wrong_input? , already_taken?
    wrong_input? return true or false depending on the mapping method return
    already_taken? true if the input(player_move) passed to it not "_"

--- lib/board.rb
+++ lib/board.rb
@@ -13,10 +13,27 @@ class Board
        "#{@board[0][0]} #{@board[0][1]} #{@board[0][2]}\n" +
        "#{@board[1][0]} #{@board[1][1]} #{@board[1][2]}\n" +
        "#{@board[2][0]} #{@board[2][1]} #{@board[2][2]}"
-        
-        
-        
      end
+
+     def check_cell(input)
+
+     end
+
+     def wrong_input?(input)
+        mapping(input) == nil 
+     end
+
+    def already_taken?(move)
+        move != '_'
+    end
+
+    def mapping(move)
+        move = move.to_sym
+        board_hash = { A1: [0, 0], A2: [0, 1], A3: [0, 2],
+                       B1: [1, 0], B2: [1, 1], B3: [1, 2],
+                       C1: [2, 0], C2: [2, 1], C3: [2, 2] }
+        board_hash[move]
+    end
 end
 
 #board1 = Board.new
