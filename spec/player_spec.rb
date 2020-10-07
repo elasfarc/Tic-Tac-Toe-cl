a3249b1f74f272613d9cd5978d42b1207764e8dd
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2020-10-07 23:21:00 +0200

    test Plyer class -> valid_name(name) method

---
+++ spec/player_spec.rb
@@ -0,0 +1,39 @@
+require_relative '../lib/player.rb'
+
+
+RSpec.describe Player do
+
+    #let(:player) { Player.new()}
+    subject {Player.new()}
+    
+    describe '#valid_name?' do
+
+        it 'player name is not an empty string' do
+            player_name = ''
+            expect(subject.valid_name?(player_name)).to eq(false)
+        end
+
+        it 'player name is not a space' do
+            player_name = ' '
+            expect(subject.valid_name?(player_name)).to eq(false)
+        end
+
+        it 'player name is not a number' do
+            player_name = '4545'
+            expect(subject.valid_name?(player_name)).to eq(false)
+        end
+
+        it 'player name is not starting with ZERO' do
+            player_name = '0player'
+            expect(subject.valid_name?(player_name)).to eq(false)
+        end
+
+        it 'player name is a valid name' do
+            player_name = 'player'
+            expect(subject.valid_name?(player_name)).to eq(true)
+        end
+        
+
+    end
+
+end
