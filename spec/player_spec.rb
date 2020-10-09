44a94aec13f297838d37a1cd25d8264d5239629d
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2020-10-09 23:21:28 +0200

    reformat player_spec testing logic

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,29 +4,41 @@ RSpec.describe Player do
   subject { Player.new }
 
   describe '#valid_name?' do
-    it 'player name is not an empty string' do
-      player_name = ''
-      expect(subject.valid_name?(player_name)).to eq(false)
+    context 'Invalid player name' do
+      context 'player name is an empty string' do
+        it 'returns false' do
+          player_name = ''
+          expect(subject.valid_name?(player_name)).to eq(false)
+        end
+      end
+
+      context 'player name is a space' do
+        it 'returns false' do
+          player_name = ' '
+          expect(subject.valid_name?(player_name)).to eq(false)
+        end
+      end
+
+      context 'player name is a number' do
+        it 'returns false' do
+          player_name = '4545'
+          expect(subject.valid_name?(player_name)).to eq(false)
+        end
+      end
+
+      context 'player name is starting with ZERO' do
+        it 'returns false' do
+          player_name = '0player'
+          expect(subject.valid_name?(player_name)).to eq(false)
+        end
+      end
     end
 
-    it 'player name is not a space' do
-      player_name = ' '
-      expect(subject.valid_name?(player_name)).to eq(false)
-    end
-
-    it 'player name is not a number' do
-      player_name = '4545'
-      expect(subject.valid_name?(player_name)).to eq(false)
-    end
-
-    it 'player name is not starting with ZERO' do
-      player_name = '0player'
-      expect(subject.valid_name?(player_name)).to eq(false)
-    end
-
-    it 'player name is a valid name' do
-      player_name = 'player'
-      expect(subject.valid_name?(player_name)).to eq(true)
+    context 'valid player name ' do
+      it 'returns true' do
+        player_name = 'player'
+        expect(subject.valid_name?(player_name)).to eq(true)
+      end
     end
   end
 end
