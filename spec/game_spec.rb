50a81ee61d88266cd654ccc0691b00d0031bb837
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2020-10-09 23:41:04 +0200

    game_spec -> reformat test for player_two_is_the_same_as_player_one? && reset

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,12 +4,21 @@ RSpec.describe Game do
   let(:game) { Game.new }
   let(:player_one) { double('player one object', name: 'Recardo') }
   let(:player_two) { double('player two object', name: 'Ted') }
-  describe '#player_two_is_the_same_as_player_one?' do
-    it 'ensures that name of player two is not the same as player one name  ' do
-      game.players_arry = [player_one]
 
-      expect(game.player_two_is_the_same_as_player_one?('Recardo')).to eq(true)
-      expect(game.player_two_is_the_same_as_player_one?('Ted')).to_not eq(true)
+  describe '#player_two_is_the_same_as_player_one?' do
+    context 'player two\'s name is the same as player one\'s name' do
+      it 'returns true' do
+        game.players_arry = [player_one]
+
+        expect(game.player_two_is_the_same_as_player_one?('Recardo')).to eq(true)
+      end
+    end
+    context 'player two\'s name is not the same as player one\'s name' do
+      it 'returns false' do
+        game.players_arry = [player_one]
+
+        expect(game.player_two_is_the_same_as_player_one?('Ted')).to_not eq(true)
+      end
     end
   end
 
@@ -65,13 +74,19 @@ RSpec.describe Game do
       expect(game.players_arry).to match_array []
     end
 
-    it 'reset game object\'s variable [@current_player] to default' do
-      game.current_player = true
-      game.reset
-      expect(game.current_player).to eq(true)
-      game.current_player = false
-      game.reset
-      expect(game.current_player).to eq(true)
+    context 'current player is true' do
+      it 'reset game object\'s variable [@current_player] to default' do
+        game.current_player = true
+        game.reset
+        expect(game.current_player).to eq(true)
+      end
+    end
+    context 'current player is false' do
+      it 'reset game object\'s variable [@current_player] to default' do
+        game.current_player = false
+        game.reset
+        expect(game.current_player).to eq(true)
+      end
     end
   end
 end
